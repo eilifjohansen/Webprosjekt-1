a609fd8129c8972ecf40f4d64514c6b288d29bba
RikhartBekkevold <ikhart.v.bekkevold@gmail.com>
2015-04-10 22:45:16 +0200

    test

---
+++ prototype1/js/login.js
@@ -0,0 +1,24 @@
+/*javascript for logging in*/
+
+
+
+//var enter = document.getElementById(submit knappen);
+//event handler
+if (enter == admin1) {
+	//open adminpage
+
+}
+else if(enter ==student1 ){
+	//open studentpage	
+
+}
+
+else if(enter == teacher){
+	//open teacherpage	
+}
+
+else { 
+	alert("Password/account name combination does not exist"); 
+} 
+
+
