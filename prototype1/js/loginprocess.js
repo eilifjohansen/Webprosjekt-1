e5b58358e818f3d5a8643555a68a6f9d9ac319f2
RikhartBekkevold <ikhart.v.bekkevold@gmail.com>
2015-04-24 19:22:51 +0200

    logo, liten og stor

--- prototype1/js/loginprocess.js
+++ prototype1/js/loginprocess.js
@@ -5,26 +5,26 @@
 
 function check(form) { /*function to check userid & password*/
 	 /*the following code checkes whether the entered userid and password are matching*/
-	 if(form.userid.value == "student" && form.pswrd.value == "111") {
-		window.open("studentpage.html", "_self")/*opens the target page while Id & password matches*/
-	  }
-
+if(form.userid.value == "student" && form.pswrd.value == "111") {
+	window.open("studentpage.html", "_self")/*opens the target page while Id & password matches*/
+}
 	else {
 	 
-		  if(form.userid.value == "admin" && form.pswrd.value == "222") {
+		if(form.userid.value == "admin" && form.pswrd.value == "222") {
 			window.open("adminpage.html", "_self")/*opens the target page while Id & password matches*/
-		  }
+		}
 
-	  else {
+		else {
 	 
-		  if(form.userid.value == "teacher" && form.pswrd.value == "333") {
-			window.open("teacherpage.html", "_self")/*opens the target page while Id & password matches*/
-		  }
+			if(form.userid.value == "teacher" && form.pswrd.value == "333") {
+				window.open("teacherpage.html", "_self")/*opens the target page while Id & password matches*/
+			}
 
-		 else {
-		   alert("Error wrong password or username")/*displays error message*/
-		 }
-	  }
-    }
+			else {
+				$("span#errorFeedback").text("Password and user name combination does not exists.");
+			  
+			}
+		}
+	}
 }
 
