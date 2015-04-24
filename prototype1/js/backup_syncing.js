ab365459365ea243e0575c6b68616b336b2ac523
RikhartBekkevold <ikhart.v.bekkevold@gmail.com>
2015-04-24 02:08:53 +0200

    Nå mulig å velge topic via menyen ikke bare pilene ved Slide'en
    La også til noen divider linjer i menyen. Trøkk på linkene med divider
    linjer i mellom for å hoppe til riktig topic.

--- prototype1/js/backup_syncing.js
+++ prototype1/js/backup_syncing.js
@@ -82,7 +82,8 @@ xmlhttp.onreadystatechange=function()
     //document.getElementById("hipWIKI").innerHTML=xmlhttp.responseText;
     }
   }
-  
+	//var seekValue;
+	//var slideNo;
   //seek to particular location in video
 	var video = parent.video.document.getElementById("video1");
  	//video.currentTime = seekValue;
@@ -304,3 +305,116 @@ function videoToPowerpointSync()
    
 
 }
+
+
+
+/*
+//NEED TO IMPROVE
+//video to power point synchronization
+function process()
+{
+	var video = document.getElementById("defaultVideo");
+	//while(1)
+	{
+		//var time = parent.video.document.getElementById("video1").currentTime;
+		
+		if(video.currentTime < 2)
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[0];
+		}
+		else if(video.currentTime < 4)
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[1];		
+		}
+		else if(video.currentTime < 6)
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[2];		
+		}
+		else if(video.currentTime < 578)
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[3];		
+		}
+		else if(video.currentTime < 930)
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[4];		
+		}
+		else if(video.currentTime < 1193)
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[5];		
+		}
+		else if(video.currentTime < 1295)
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[6];		
+		}
+		else if(video.currentTime < 2080)
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[7];		
+		}								
+		else if(video.currentTime < 2480)
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[8];		
+		}
+		else
+		{
+			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[14];		
+		}
+	}
+}
+
+function videoToPowerpointSync()
+
+{ 
+
+//  var result2 = document.getElementById('result2'); 
+
+  var start = new Date().getTime(); 
+
+  var i = 0, limit = 2000000, busy = false; 
+
+  var processor = setInterval(function() 
+
+  { 
+
+    if(!busy) 
+
+    { 
+
+      busy = true; 
+
+       
+
+//      result2.value =  'time=' +  
+
+  //      (new Date().getTime() - start) + ' [i=' + i + ']'; 
+
+       
+
+      process(); 
+
+       
+
+      if(++i == limit) 
+
+      { 
+
+        clearInterval(processor); 
+
+ 
+
+  //      result2.value = 'time=' +  
+
+//          (new Date().getTime() - start) + ' [done]'; 
+
+      } 
+
+       
+
+      busy = false; 
+
+    } 
+
+     
+
+  }, 1000); 
+}
+*/
--- prototype1/js/mainscript.js
+++ prototype1/js/mainscript.js
@@ -138,112 +138,23 @@ function prevTranscript()
 
 
 
-//NEED TO IMPROVE
-//video to power point synchronization
-function process()
-{
+
+
+function jumpToTopic(contentNo, seekValue){
+
+  //seek to particular location in video
 	var video = document.getElementById("defaultVideo");
-	//while(1)
-	{
-		//var time = parent.video.document.getElementById("video1").currentTime;
-		
-		if(video.currentTime < 2)
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[0];
-		}
-		else if(video.currentTime < 4)
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[1];		
-		}
-		else if(video.currentTime < 6)
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[2];		
-		}
-		else if(video.currentTime < 578)
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[3];		
-		}
-		else if(video.currentTime < 930)
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[4];		
-		}
-		else if(video.currentTime < 1193)
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[5];		
-		}
-		else if(video.currentTime < 1295)
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[6];		
-		}
-		else if(video.currentTime < 2080)
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[7];		
-		}								
-		else if(video.currentTime < 2480)
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[8];		
-		}
-		else
-		{
-			parent.powerpoint.document.getElementById('defaultSlide').src=imageArray[14];		
-		}
-	}
+ 	//video.currentTime = seekValue;
+	video.currentTime = seekValue;
+	//parent.video.document.getElementById("video1").play();
+	video.play();
+	
+ 	//load particular slide
+	document.getElementById('defaultSlide').src= imageArray[contentNo];
+	document.getElementById('defaultWiki').src= wikiArray[contentNo];
+	document.getElementById('transcript').src= transcriptArray[contentNo];
+
+
 }
 
-function videoToPowerpointSync()
-
-{ 
-
-//  var result2 = document.getElementById('result2'); 
-
-  var start = new Date().getTime(); 
-
-  var i = 0, limit = 2000000, busy = false; 
-
-  var processor = setInterval(function() 
-
-  { 
-
-    if(!busy) 
-
-    { 
-
-      busy = true; 
-
-       
-
-//      result2.value =  'time=' +  
-
-  //      (new Date().getTime() - start) + ' [i=' + i + ']'; 
-
-       
-
-      process(); 
-
-       
-
-      if(++i == limit) 
-
-      { 
-
-        clearInterval(processor); 
-
- 
-
-  //      result2.value = 'time=' +  
-
-//          (new Date().getTime() - start) + ' [done]'; 
-
-      } 
-
-       
-
-      busy = false; 
-
-    } 
-
-     
-
-  }, 1000); 
-}
 
